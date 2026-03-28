3de126c4a112bb831ce355a66d0d9a2d4adaa956
Ben Hardill <hardillb@gmail.com>
2026-03-28 17:01:09 +0000

    Add support for listing users when no specific user provided

--- fingerd.go
+++ fingerd.go
@@ -1,92 +1,103 @@
 package main
 
 import (
-  "io"
-  "os"
-  "fmt"
-  "net"
-  "path/filepath"
-  "time"
-  "strings"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 const (
-  CONN_HOST = "0.0.0.0"
-  CONN_PORT = "79"
-  CONN_TYPE = "tcp"
+	CONN_HOST = "0.0.0.0"
+	CONN_PORT = "79"
+	CONN_TYPE = "tcp"
 )
 
-func main () {
-  l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-  if err != nil {
-    fmt.Println("Error opening port: ", err.Error())
-    os.Exit(1)
-  }
+func main() {
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error opening port: ", err.Error())
+		os.Exit(1)
+	}
 
-  defer l.Close()
-  for {
-    conn, err := l.Accept()
-    if err != nil {
-      fmt.Println("Error accepting connection: ", err.Error())
-      continue
-    }
-    go handleRequest(conn)
-  }
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleRequest(conn)
+	}
 }
 
 func check(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func handleRequest(conn net.Conn) {
-  defer conn.Close()
-  currentTime := time.Now()
-  buf := make([]byte, 1024)
-  reqLen, err := conn.Read(buf)
-  if err != nil {
-    fmt.Println("Error reading from: ", err.Error())
-  } else {
-    // fmt.Println("Connection from: ", conn.RemoteAddr())
-  }
+	defer conn.Close()
+	currentTime := time.Now()
+	buf := make([]byte, 1024)
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from: ", err.Error())
+	} else {
+		// fmt.Println("Connection from: ", conn.RemoteAddr())
+	}
 
-  request := strings.TrimSpace(string(buf[:reqLen]))
-  fmt.Println(currentTime.Format(time.RFC3339), conn.RemoteAddr(), request)
+	request := strings.TrimSpace(string(buf[:reqLen]))
+	fmt.Println(currentTime.Format(time.RFC3339), conn.RemoteAddr(), request)
 
-  parts := strings.Split(request, " ")
-  wide := false
-  user := parts[0]
+	parts := strings.Split(request, " ")
+	wide := false
+	user := parts[0]
 
-  if parts[0] == "/W" && len(parts) == 2 {
-    wide = true
-    user = parts[1]
-  } else if parts[0] == "/W" && len(parts) == 1 {
-    conn.Write([]byte("\r\n"))
-    return
-  }
+	if parts[0] == "/W" && len(parts) == 2 {
+		wide = true
+		user = parts[1]
+	} else if parts[0] == "/W" && len(parts) == 1 {
+		conn.Write([]byte("\r\n"))
+		return
+	}
 
-  if strings.Index(user, "@") != -1 {
-    conn.Write([]byte("Forwarding not supported\r\n"))
-  } else {
-    if wide {
-      //TODO
-    } else {
-      pwd, err := os.Getwd()
-      filePath := filepath.Join(pwd, "plans", filepath.Base(user + ".plan"))
-      filePath, err = filepath.Abs(filePath)
-      check(err)
-      if filepath.Dir(filePath) == filepath.Join(pwd, "plans") {
-        file, err := os.Open(filePath)
-        if err != nil {
-          //not found
-          // io.Write([]byte("Not Found\r\n"))
-        } else {
-          defer file.Close()
-          io.Copy(conn,file)
-          conn.Write([]byte("\r\n"))
-        }
-      }
-    }
-  }
+	if strings.Index(user, "@") != -1 {
+		conn.Write([]byte("Forwarding not supported\r\n"))
+	} else {
+		if wide {
+			//TODO
+		} else if user == "" {
+			pwd, err := os.Getwd()
+			entries, err := os.ReadDir(filepath.Join(pwd, "plans"))
+			check(err)
+			conn.Write([]byte("Username:\r\n"))
+			for _, ent := range entries {
+				u := strings.Split(ent.Name(), ".")
+				conn.Write([]byte(u[0]))
+				conn.Write([]byte("\r\n"))
+			}
+			conn.Write([]byte("\r\n"))
+		} else {
+			pwd, err := os.Getwd()
+			filePath := filepath.Join(pwd, "plans", filepath.Base(user+".plan"))
+			filePath, err = filepath.Abs(filePath)
+			check(err)
+			if filepath.Dir(filePath) == filepath.Join(pwd, "plans") {
+				file, err := os.Open(filePath)
+				if err != nil {
+					//not found
+					// io.Write([]byte("Not Found\r\n"))
+				} else {
+					defer file.Close()
+					io.Copy(conn, file)
+					conn.Write([]byte("\r\n"))
+				}
+			}
+		}
+	}
 }
